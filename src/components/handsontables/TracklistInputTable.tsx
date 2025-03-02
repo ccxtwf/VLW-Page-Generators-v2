55226bb48ded073d1ec9e3a5376ce7f23e889819
ccxtwf <theweirdfandom@gmail.com>
2025-03-02 10:52:23 +0700

    FEAT: Context menu option: Add markup

--- src/components/handsontables/TracklistInputTable.tsx
+++ src/components/handsontables/TracklistInputTable.tsx
@@ -1,7 +1,6 @@
-import { ForwardedRef, forwardRef } from "react";
+import { ForwardedRef, forwardRef, useMemo } from "react";
 // @ts-ignore
 import { HotTable } from '@handsontable/react';
-import { sharedContextMenuOptions } from "./shared";
 import { CONST_WIKI_DOMAIN } from "../../constants/linkDomains";
 
 const TracklistInputTable = forwardRef(function TracklistInputTable(
@@ -39,6 +38,164 @@ const TracklistInputTable = forwardRef(function TracklistInputTable(
   ];
   let columnWidths = [5, 5, 50, 20, 20];
 
+  const rxListSeparator = useMemo(() => (
+    /(,\s*(?!and\b)|,?\s+and\s+|\s+&\s+)/
+  ), []);
+
+  const contextMenuOptions = useMemo(() => ({
+    items: {
+      copy: { disabled: false },
+      cut: { disabled: false },
+      paste: {
+        name: 'Paste',
+        //@ts-ignore
+        callback(key, selection, clickEvent) {
+          let { 
+            start: { row: fromRow = null, col: fromCol = null } = {}, 
+            end: { row: toRow = null, col: toCol = null } = {} 
+          } = (selection || [{}])[0];
+          if (fromRow === null || fromCol === null || toRow === null || toCol === null) return;
+          if (fromRow > toRow) [fromRow, toRow] = [toRow, fromRow];
+          if (fromCol > toCol) [fromCol, toCol] = [toCol, fromCol];
+          navigator.clipboard.readText()
+            .then((str) => {
+              const pasted = str.split(/\n/).map((line) => line.split(/\t/));
+
+              //@ts-ignore
+              // let data = this.getData();
+              let numExistingRows = this.getData().length;
+              let numOverlappedExistingRows = Math.min(
+                numExistingRows - fromRow,
+                pasted.length
+              );
+              let numOverflowedRows = pasted.length - numOverlappedExistingRows;
+              const changes: (number | string)[][] = [];
+
+              if (fromRow === toRow && fromCol === toCol) {
+                // Starting cell is a single cell
+                // In this case, limit the paste range to rows after fromRow and columns after fromCol
+                let numOverlappedExistingColumns = Math.min(
+                  5, Math.max(...pasted.map(line => line.length))
+                );
+                for (let i = 0; i < numOverlappedExistingRows; i++) {
+                  for (let j = 0; j < numOverlappedExistingColumns; j++) {
+                    changes.push([fromRow+i, fromCol+j, pasted[i][j] || '']);
+                  }
+                }
+                for (let i = 0; i < numOverflowedRows; i++) {
+                  for (let j = 0; j < numOverlappedExistingColumns; j++) {
+                    changes.push([numExistingRows+i, fromCol+j, pasted[numOverlappedExistingRows+i][j] || '']);
+                  }
+                }
+              } else {
+                // Starting cell is a multi-cell range
+                // In this case, limit the paste range to the columns within fromCol & toCol, 
+                // and to rows below (equal to or more than) fromRow
+                for (let i = 0; i < numOverlappedExistingRows; i++) {
+                  for (let j = fromCol; j <= toCol; j++) {
+                    changes.push([fromRow+i, fromCol+j, pasted[i][j-fromCol] || '']);
+                  }
+                }
+                for (let i = 0; i < numOverflowedRows; i++) {
+                  for (let j = fromCol; j <= toCol; j++) {
+                    changes.push([numExistingRows+i, fromCol+j, pasted[numOverlappedExistingRows+i][j-fromCol] || '']);
+                  }
+                }
+              }
+
+              //@ts-ignore
+              this.setDataAtCell(changes);
+            })
+            .catch(() => {
+              window.alert("Unable to paste! Please use keyboard command (Ctrl/Cmd + V)!");
+            })
+        }
+      },
+      sp1: '---------',
+      undo: { disabled: false },
+      redo: { disabled: false },
+      sp2: '---------',
+      producerMarkup: {
+        name: () => 'Add producer markup',
+        hidden() {
+          const { 
+            from: { col: fromCol = null } = {}, 
+            to: { col: toCol = null } = {} 
+          //@ts-ignore
+          } = (this.getSelectedRange() || [{}])[0];
+          if (fromCol !== 3 || toCol !== 3) return true;
+          return false;
+        },
+        //@ts-ignore
+        callback(key, selection, clickEvent) {
+          const { 
+            start: { row: fromRow = null, col: fromCol = null } = {}, 
+            end: { row: toRow = null, col: toCol = null } = {} 
+          } = (selection || [{}])[0];
+          if (fromRow === null || toRow === null) return;
+          if (fromCol !== 3 || toCol !== 3) return;
+          //@ts-ignore
+          const data = this.getData();
+          for (let i = fromRow; i <= toRow; i++) {
+            const row = data[i];
+            let producer = (row[3] || '').trim();
+            let producerSplit = producer.split(rxListSeparator);
+            console.log(producerSplit);
+            producer = '';
+            for (let substring of producerSplit) {
+              if (!substring.match(rxListSeparator) && substring !== '') substring = `[[${substring}]]`;
+              producer += substring;
+            }
+            row[3] = producer;
+          }
+          //@ts-ignore
+          this.loadData(data);
+        },
+      },
+      singerMarkup: {
+        name: () => 'Add singer markup',
+        hidden() {
+          const { 
+            from: { col: fromCol = null } = {}, 
+            to: { col: toCol = null } = {} 
+          //@ts-ignore
+          } = (this.getSelectedRange() || [{}])[0];
+          if (fromCol !== 4 || toCol !== 4) return true;
+          return false;
+        },
+        //@ts-ignore
+        callback(key, selection, clickEvent) {
+          const { 
+            start: { row: fromRow = null, col: fromCol = null } = {}, 
+            end: { row: toRow = null, col: toCol = null } = {} 
+          } = (selection || [{}])[0];
+          if (fromRow === null || toRow === null) return;
+          if (fromCol !== 4 || toCol !== 4) return;
+          //@ts-ignore
+          const data = this.getData();
+          for (let i = fromRow; i <= toRow; i++) {
+            const row = data[i];
+            let singer = (row[4] || '').trim();
+            let singerSplit = singer.split(rxListSeparator);
+            singer = '';
+            for (let substring of singerSplit) {
+              if (!substring.match(rxListSeparator) && substring !== '') substring = `[[${substring}]]`;
+              singer += substring;
+            }
+            row[4] = singer;
+          }
+          //@ts-ignore
+          this.loadData(data);
+        },
+      },
+      sp3: '---------',
+      row_above: { disabled: false }, 
+      row_below: { disabled: false }, 
+      remove_row: { disabled: false },
+      clear_column: { disabled: false }
+    }
+  }), []);
+
   const handleVlwPageUrlInputEvent = (changes: (any[] | null)[]) => {
     for (let change of changes) {
       // @ts-ignore
@@ -65,7 +222,7 @@ const TracklistInputTable = forwardRef(function TracklistInputTable(
         colHeaders={headerText}
         columns={columnDefinitions}
         width="100%"
-        contextMenu={sharedContextMenuOptions}
+        contextMenu={contextMenuOptions}
         autoWrapRow={true}
         autoWrapCol={true}
         manualColumnResize={true}
