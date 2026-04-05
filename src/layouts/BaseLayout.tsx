8489ae1557e849519f6f7713f8ac650e1b52988f
ccxtwf <theweirdfandom@gmail.com>
2026-04-05 18:13:36 +0700

    FIX: Remove migration notice

--- src/layouts/BaseLayout.tsx
+++ src/layouts/BaseLayout.tsx
@@ -1,7 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import { useBeforeunload } from 'react-beforeunload';
 
-import MigrationNotice from '../components/reusables/MigrationNotice';
 import GlobalHeader from '../components/GlobalHeader';
 import Navbar from '../components/Navbar';
 import GlobalFooter from '../components/GlobalFooter';
@@ -15,7 +14,6 @@ export default function BaseLayout() {
       <GlobalHeader />
       <Navbar />
       <main>
-        <MigrationNotice />
         <Outlet />
       </main>
       <hr />
